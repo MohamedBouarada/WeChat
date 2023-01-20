501d64c21bb02a8d7aa6ff598b600107a5d716f2
MohamedBouarada <bouarada7.mb@gmail.com>
2023-01-20 02:53:43 +0100

    encrypt and decrypt with rsa

---
+++ rsa_handler.py
@@ -0,0 +1,58 @@
+from Crypto.PublicKey import RSA
+import base64
+from cryptography import x509
+from cryptography.hazmat.primitives.asymmetric import padding
+from Crypto.Cipher import PKCS1_v1_5
+
+from cryptography.hazmat.backends import default_backend
+from Crypto.Random import get_random_bytes
+
+from dotenv.main import load_dotenv
+import os
+
+load_dotenv()
+
+CA_CLIENT_CERT_DIR=os.environ['CA_CLIENT_CERT_DIR']
+CA_CLIENT_KEY_DIR=os.environ['CA_CLIENT_KEY_DIR']
+
+def rsa_encrypt(message, receiver_username):
+    
+    cert = x509.load_pem_x509_certificate(open(CA_CLIENT_CERT_DIR+receiver_username+"_cert.pem", 'rb').read(), default_backend())
+    pub_key=cert.public_key()
+    print(pub_key)
+    message = str.encode(message)
+    #rsa_public_key = RSA.importKey(pub_key)
+    #rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+    #encrypted_message = rsa_public_key.encrypt(message)
+    encrypted_message = pub_key.encrypt(message,padding.PKCS1v15())
+
+    encrypted_message = base64.b64encode(encrypted_message)
+    return encrypted_message
+
+
+def rsa_decrypt(encrypted_message, receiver_username):
+    
+    rsa_private_key = RSA.importKey(open(CA_CLIENT_KEY_DIR+receiver_username+".key","r").read())
+    rsa_private_key = PKCS1_v1_5.new(rsa_private_key)
+    encrypted_message = base64.b64decode(encrypted_message)
+    sentinel = get_random_bytes(16)
+    decrypted_message = rsa_private_key.decrypt(encrypted_message,sentinel)
+    return decrypted_message
+
+
+
+# get rsa key from file
+# def get_rsa_key(filepath):
+#     with open(filepath, mode='rb') as private_file:
+#         priv_key_data = private_file.read()
+#         private_key = RSA.importKey(priv_key_data)
+#         #print(private_key.export_key())
+#         return private_key
+
+
+#test:
+
+# encrypt=rsa_encrypt("hello","cert")
+# print(encrypt ,'\n')
+# decrypt = rsa_decrypt(encrypt, "cert")
+# print(decrypt)
